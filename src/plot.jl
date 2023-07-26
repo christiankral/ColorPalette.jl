a606fda57951885da51f89799f7997e41951f0df
Christian Kral <dr.christian.kral@gmail.com>
2023-07-26 12:36:17 +0200

    Remove axis

--- src/plot.jl
+++ src/plot.jl
@@ -1,4 +1,17 @@
-function PlotPalette(color; save = false)
+export plotpalette
+
+function removeaxis(ax=gca())
+    setproperty!(ax,"set_axis_off",true)
+    xticks([])
+    yticks([])
+    ax.spines["right"].set_visible(false)
+    ax.spines["top"].set_visible(false)
+    ax.spines["bottom"].set_visible(false)
+    ax.spines["left"].set_visible(false)
+    axis("square")
+end
+
+function plotpalette(color; save = false)
     figure()
     for k in 1:size(color,1)
         for l in 1:size(color,2)
@@ -6,6 +19,7 @@ function PlotPalette(color; save = false)
             fill([l,l-1,l-1,l], [k-1,k-1,k,k], color = c)
         end
     end
+    removeaxis()
     if save
         savefig("color.png",dpi=300)
     end
@@ -17,6 +31,7 @@ function PlotPalette(color; save = false)
             fill([l,l-1,l-1,l], [k-1,k-1,k,k], color = color2gray(c))
         end
     end
+    removeaxis()
     if save
         savefig("gray.png",dpi=300)
     end
