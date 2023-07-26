0b5a01d7ad729dd47064144adf412a0f64de94f9
Christian Kral <dr.christian.kral@gmail.com>
2023-07-26 17:35:49 +0200

    Add Ipe color palettes

--- resources/ipe/ln.sh
+++ resources/ipe/ln.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 # Symbolic link to IPE styles directory
+ln -sf "${PWD}/Contrast.isy" "${HOME}/.ipe/styles/Contrast.isy"
+ln -sf "${PWD}/Coolors.isy" "${HOME}/.ipe/styles/Coolors.isy"
+ln -sf "${PWD}/Hanser2.isy" "${HOME}/.ipe/styles/Hanser2.isy"
+ln -sf "${PWD}/Hanser.isy" "${HOME}/.ipe/styles/Hanser.isy"
+ln -sf "${PWD}/Purple.isy" "${HOME}/.ipe/styles/Purple.isy"
 ln -sf "${PWD}/Seasonal.isy" "${HOME}/.ipe/styles/Seasonal.isy"
