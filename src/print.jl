d64e4db8d818d03cf92fbab129b40e7be609a41d
Christian Kral <dr.christian.kral@gmail.com>
2023-07-26 17:28:14 +0200

    Optionally print results to files

--- src/print.jl
+++ src/print.jl
@@ -1,49 +1,58 @@
 export printcoloripe, printcolorlatex
 
 function printcoloripe(color; name="")
+    # Write to file
     if name != ""
-        println("<?xml version=\"1.0\"?>")
-        println("<!DOCTYPE ipestyle SYSTEM \"ipe.dtd\">")
-        println("<ipestyle name=\""*name*"\">")
+        file = open(name*".isy", "w")
+        println(file, "<?xml version=\"1.0\"?>")
+        println(file, "<!DOCTYPE ipestyle SYSTEM \"ipe.dtd\">")
+        println(file, "<ipestyle name=\""*name*"\">")
     end
     for k in 1:size(color,1)
         for l in 1:size(color,2)
             c = color[k,l]
-            r, g, b = color2rgb(c)
+            r, g, b = round.(color2rgb(c), sigdigits=4)
 
             # Print color code in Ipe format
             str = "  <color name=\"color"*string(k)*string(l)*"\" value=\""*string(r)*" "*string(g)*" "*string(b)*"\"/>"
-            println(str)
+            if name != ""
+                println(file, str)
+            else
+                println(str)
+            end
         end
     end
     if name != ""
-        println("</ipestyle>")
+        println(file, "</ipestyle>")
+        close(file)
     end
 end
 
 
 function printcolorlatex(color; name="")
     if name != ""
+        file = open(name*".tex", "w")
+        println(file, "% Color palette: "*name)
+        println(file, "\\usepackage{xcolor}")
+    else
         println("% Color palette: "*name)
         println("\\usepackage{xcolor}")
     end
     for k in 1:size(color,1)
         for l in 1:size(color,2)
             c = color[k,l]
-            hex_value = parse(UInt, c[2:end], base=16)
-            # Color code 0 ≤ red|green|blue ≤ 1
-            red = (hex_value >> 16) & 0xFF
-            green = (hex_value >> 8) & 0xFF
-            blue = hex_value & 0xFF
+            R, G, B = color2RGB(c)
 
-            # Color code 0 ≤ r|g|b ≤ 1
-            r = string(round(red/255,sigdigits=4))
-            g = string(round(green/255,sigdigits=4))
-            b = string(round(blue/255,sigdigits=4))
-
-            # Print color code in Ipe format
-            str = "\\definecolor{color"*string(k)*string(l)*"}{RGB}{"*string(red)*","*string(green)*","*string(blue)*"}"
-            println(str)
+            # Print color code in LaTeX format
+            str = "\\definecolor{color"*string(k)*string(l)*"}{RGB}{"*string(R)*","*string(G)*","*string(B)*"}"
+            if name != ""
+                println(file, str)
+            else
+                println(str)
+            end
         end
+    end
+    if name != ""
+        close(file)
     end
 end
