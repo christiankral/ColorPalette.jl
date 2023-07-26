163d0575d431c272ebeae2d7d627b1e699b2bcd5
Christian Kral <dr.christian.kral@gmail.com>
2023-07-26 11:49:29 +0200

    Change green and format for palettes

--- src/palettes.jl
+++ src/palettes.jl
@@ -1,7 +1,8 @@
 export color_seasonal, color_contrast
 
 # https://www.schemecolor.com/only-seasonal.php
-color_seasonal = [["#312b59" "#5c5380" "#887ea8" "#b6acd3" "#e6ddff"];
+color_seasonal = [
+    ["#312b59" "#5c5380" "#887ea8" "#b6acd3" "#e6ddff"];
     ["#853277" "#a35b95" "#c184b4" "#dfadd4" "#fdd7f4"];
     ["#7aa337" "#93b45b" "#abc57d" "#c3d69f" "#dbe7c2"];
     ["#d37f2a" "#e09651" "#ecad75" "#f7c49a" "#ffdcc0"];
@@ -9,26 +10,30 @@ color_seasonal = [["#312b59" "#5c5380" "#887ea8" "#b6acd3" "#e6ddff"];
 
 # First and fourth color of https://www.schemecolor.com/only-seasonal.php
 # In combination with https://www.learnui.design/tools/data-color-picker.html#palette
-color_contrast = [["#312B59" "#5c5380" "#887ea8" "#b6acd3" "#e6ddff"];
+color_contrast = [
+    ["#312B59" "#5c5380" "#887ea8" "#b6acd3" "#e6ddff"];
     ["#82396e" "#a15f8d" "#c086ae" "#dfaecf" "#ffd7f2"];
     ["#c94f65" "#d97281" "#e7939e" "#f4b4bc" "#ffd5da"];
     ["#f1814a" "#f79867" "#fbae86" "#fec3a5" "#ffd9c5"];
     ["#f0c435" "#f3cb5c" "#f4d27d" "#f4da9d" "#f3e1bc"]]
 
-color_coolors = [["#0f4c5c"];
+color_coolors = [
+    ["#0f4c5c"];
     ["#5f0f40"];
     ["#9a031e"];
     ["#e36414"];
     ["#fb8b24"]]
 
-color_purple = [["#3f1551"];
+color_purple = [
+    ["#3f1551"];
     ["#653780"];
     ["#9c3587"];
     ["#e53f71"];
     ["#f8955b"]]
 
-color_hanser = [["#204786"];
+color_hanser = [
+    ["#204786"];
     ["#d71d2b"];
     ["#853277"];
-    ["#789048"];
+    ["#598234"];
     ["#f68712"]]
