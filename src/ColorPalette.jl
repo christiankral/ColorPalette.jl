18ec324f62f4a4017f7e6962a837b31d41275b85
Christian Kral <dr.christian.kral@gmail.com>
2023-07-27 18:32:27 +0200

    Documentation of available color palettes

--- src/ColorPalette.jl
+++ src/ColorPalette.jl
@@ -1,4 +1,22 @@
 __precompile__(true)
+"""
+# ColorPalette
+
+Available color palettes do alway consist of
+- five colors with
+- five hues each
+
+The color palette names are:
+- `black` Solely black colors for prurely black and white appearance
+- `gray` Gray shades
+- `purple` Purple colors with no green
+- `purple2` Alternative purple colors with no green
+- `hanser` Color palette matching technical book publications of the Hanser Verlag
+- `hanser2` Alternative color palette matching technical book publications of the Hanser Verlag
+
+Color and gray shade samples are available in the directory `resources/png`
+
+"""
 module ColorPalette
 
     using Printf
