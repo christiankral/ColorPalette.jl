c242cee6ead2974fa1b8c4b14a190e71dacdc63f
Christian Kral <dr.christian.kral@gmail.com>
2023-07-27 23:11:05 +0200

    Update documentation of color palettes

--- src/ColorPalette.jl
+++ src/ColorPalette.jl
@@ -11,11 +11,12 @@ The color palette names are:
 - `black` Solely black colors for prurely black and white appearance
 - `gray` Gray shades
 - `purple` Purple colors with no green
-- `purple2` Alternative purple colors with no green
+- `purple2` Second alternative purple colors with no green
 - `hanser` Color palette matching technical book publications of the Hanser Verlag
-- `hanser2` Alternative color palette matching technical book publications of the Hanser Verlag
+- `hanser2` Second alternative color palette matching technical book publications of the Hanser Verlag
+- `hanser3` Third alternative color palette matching technical book publications of the Hanser Verlag
 
-Each color palette is a 5 x 5 matrix of hex color strings. 
+Each color palette is a 5 x 5 matrix of hex color strings.
 
 Color and gray shade samples are available in the directory `resources/png`
 
