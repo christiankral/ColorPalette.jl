bd5e1730ff75a8963a46f90c3e81ea68c01efa38
Christian Kral <dr.christian.kral@gmail.com>
2023-07-27 11:23:27 +0200

    Update resource names

--- resources/ipe/ln.sh
+++ resources/ipe/ln.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Symbolic link to IPE styles directory
-ln -sf "${PWD}/Contrast.isy" "${HOME}/.ipe/styles/Contrast.isy"
-ln -sf "${PWD}/Coolors.isy" "${HOME}/.ipe/styles/Coolors.isy"
-ln -sf "${PWD}/Hanser2.isy" "${HOME}/.ipe/styles/Hanser2.isy"
-ln -sf "${PWD}/Hanser.isy" "${HOME}/.ipe/styles/Hanser.isy"
-ln -sf "${PWD}/Purple.isy" "${HOME}/.ipe/styles/Purple.isy"
-ln -sf "${PWD}/Seasonal.isy" "${HOME}/.ipe/styles/Seasonal.isy"
+ln -sf "${PWD}/Seasonal.isy" "${HOME}/.ipe/styles/ColorPalette.seasonal.isy"
+ln -sf "${PWD}/Coolors.isy" "${HOME}/.ipe/styles/ColorPalette.coolors.isy"
+ln -sf "${PWD}/Purple.isy" "${HOME}/.ipe/styles/ColorPalette.purple.isy"
+ln -sf "${PWD}/Contrast.isy" "${HOME}/.ipe/styles/ColorPalette.purple2.isy"
+ln -sf "${PWD}/Hanser.isy" "${HOME}/.ipe/styles/ColorPalette.hanser.isy"
+ln -sf "${PWD}/Hanser2.isy" "${HOME}/.ipe/styles/ColorPalette.hanser2.isy"
