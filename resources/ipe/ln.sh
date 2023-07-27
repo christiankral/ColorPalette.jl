e4f70375559154c31d9448e36499ceecee873cef
Christian Kral <dr.christian.kral@gmail.com>
2023-07-27 11:26:45 +0200

    Fix bug in linking script

--- resources/ipe/ln.sh
+++ resources/ipe/ln.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Symbolic link to IPE styles directory
-ln -sf "${PWD}/Seasonal.isy" "${HOME}/.ipe/styles/ColorPalette.seasonal.isy"
-ln -sf "${PWD}/Coolors.isy" "${HOME}/.ipe/styles/ColorPalette.coolors.isy"
-ln -sf "${PWD}/Purple.isy" "${HOME}/.ipe/styles/ColorPalette.purple.isy"
-ln -sf "${PWD}/Contrast.isy" "${HOME}/.ipe/styles/ColorPalette.purple2.isy"
-ln -sf "${PWD}/Hanser.isy" "${HOME}/.ipe/styles/ColorPalette.hanser.isy"
-ln -sf "${PWD}/Hanser2.isy" "${HOME}/.ipe/styles/ColorPalette.hanser2.isy"
+ln -sf "${PWD}/ColorPalette.seasonal.isy" "${HOME}/.ipe/styles/ColorPalette.seasonal.isy"
+ln -sf "${PWD}/ColorPalette.coolors.isy" "${HOME}/.ipe/styles/ColorPalette.coolors.isy"
+ln -sf "${PWD}/ColorPalette.purple.isy" "${HOME}/.ipe/styles/ColorPalette.purple.isy"
+ln -sf "${PWD}/ColorPalette.purple2.isy" "${HOME}/.ipe/styles/ColorPalette.purple2.isy"
+ln -sf "${PWD}/ColorPalette.hanser.isy" "${HOME}/.ipe/styles/ColorPalette.hanser.isy"
+ln -sf "${PWD}/ColorPalette.hanser2.isy" "${HOME}/.ipe/styles/ColorPalette.hanser2.isy"
